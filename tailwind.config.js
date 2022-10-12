eb2dfc3ea8622bc53e67471723e1abff815aa4f2
rg3915 <regis42santos@gmail.com>
2022-10-12 03:25:50 -0300

    Configura extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: '#c542f5',
+          200: '#9e35c4',
+          300: '#6d2487',
+          400: '#331040'
+        }
+      }
+    },
   },
   plugins: [],
 }
